cb37ee996ee3007c04cf5d9b578f5633e17edeb2
Kazuyuki SUZUKI <kechol@users.noreply.github.com>
2019-11-14 22:08:25 +0900

    Allow lvh.me

--- hook/proxy.rb
+++ hook/proxy.rb
@@ -2,7 +2,7 @@ r = Nginx::Request.new
 c = Nginx::Connection.new
 me = Docker::Container.me!
 
-if container = Docker::Container.find_by_fqdn(r.hostname)
+if container = Docker::Container.find_by_hostname(r.hostname)
   not_connected_networks = (container.networks - me.networks)
   not_connected_networks.each do |n|
     n.connect(me)
@@ -10,7 +10,7 @@ if container = Docker::Container.find_by_fqdn(r.hostname)
   if not_connected_networks.any?
     Docker::Container.expire_cache!
     me = Docker::Container.me!
-    container = Docker::Container.find_by_fqdn(r.hostname)
+    container = Docker::Container.find_by_hostname(r.hostname)
   end
 
   "#{container.ip_address(me)}:#{c.local_port}"
